669c7c0f5ef932d691eb3e46254e9860fcbec231
alan <alan001tk@gmail.com>
2022-08-02 14:54:50 -0700

    Finish myEach in phase_2

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -1,5 +1,11 @@
-Array.prototype.twoSum = function {
-    //return a array of position pairs where elements would sum to 0
-    const bigArray = [];
-
+Array.prototype.myEach = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    };
 };
+
+let arr = [1, 2, 3, 4, 5];
+// arr.myEach(console.log);
+// arr.myEach(function(el) {console.log(el + 3)});
+
+ 

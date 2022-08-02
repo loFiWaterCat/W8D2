cc45dbfab4cca17666ef4bbc8433302a04a1774d
bob-skywalker <q463250938@gmail.com>
2022-08-02 14:46:07 -0700

    finished transpose

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,14 +1,48 @@
-Array.prototype.uniq = function() {
-  const bank = [];
+// Array.prototype.uniq = function() {
+//   const bank = [];
 
-  this.forEach((el) => {
-    if (!bank.includes(el)) {
-      bank.push(el);
+//   this.forEach((el) => {
+//     if (!bank.includes(el)) {
+//       bank.push(el);
+//     };
+//   });
+//   return bank;
+// };
+
+
+// dups = [1, 2, 2, 3, 5, 4, 3, 5, 5, 4];
+// console.log(dups.uniq());
+
+// Array.prototype.twoSum = function() {
+//   const bigArray = [];
+
+//   this.forEach(el1 => {
+//     this.forEach(el2 =>{
+//       if(this.indexOf(el2) > this.indexOf(el1) && el1 + el2 === 0) {
+//         bigArray.push([this.indexOf(el1), this.indexOf(el2)]);
+//       }
+//     });
+
+//   });
+//   return bigArray;
+// };
+
+
+// dups = [1,2,-1,5,4,-3,3,-5,0];
+// console.log(dups.twoSum());
+
+
+Array.prototype.transpose = function(){
+  const bigArr = [];
+  for (let j = 0; j < this[0].length; j++){
+    const subArr = [];
+    for (let i = 0; i < this.length; i ++){
+     subArr.push(this[i][j]);
     };
-  });
-  return bank;
+  bigArr.push(subArr);
+  };
+  return bigArr;
 };
 
-
-dups = [1, 2, 2, 3, 5, 4, 3, 5, 5, 4];
-console.log(dups.uniq());
+two_D = [[1,5,3,2],[3,5,7,2],[7,2,1,6]];
+console.log(two_D.transpose());
---
+++ skeleton/phase_2_enumerable.js
@@ -0,0 +1,5 @@
+Array.prototype.twoSum = function {
+    //return a array of position pairs where elements would sum to 0
+    const bigArray = [];
+
+};
